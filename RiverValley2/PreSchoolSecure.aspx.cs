b078097dd129d8a296c1ac586f08578a4319dd78
Jude <judek15@gmail.com>
2016-03-16 10:32:47 -0500

    Work in progress

---
+++ RiverValley2/PreSchoolSecure.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RiverValley2
+{
+    public partial class PreSchoolSecure : RiverValleyPage2
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LabelMain.Text = GetContent("Body");
+            LiteralPictures.Text = GenerateRandomPictures();
+        }
+    }
+}
